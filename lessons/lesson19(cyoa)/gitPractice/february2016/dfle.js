cd3c9339a7d4c451c7fcb02f0e5d946592a252d3
dfle <diannefle@gmail.com>
2016-02-25 20:05:22 -0800

    letter capitalize coderbyte

---
+++ lessons/lesson19(cyoa)/gitPractice/february2016/dfle.js
@@ -0,0 +1,17 @@
+/* 
+Letter Capitalize
+https://coderbyte.com/editor/guest:Letter%20Capitalize:JavaScript
+
+Using the JavaScript language, have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. Words will be separated by only one space. 
+
+Use the Parameter Testing feature in the box below to test your code with different arguments.*/
+
+function LetterCapitalize(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
+   
+// keep this function call here 
+LetterCapitalize(readline());   
